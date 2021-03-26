c032ee5e0632b17e3d3c660a87c88a6870a5b360
Angie Todd <angela.todd@autofinance.com>
2021-03-25 20:40:05 -0400

    Initialized class_notes repo

---
+++ html_lesson/day_2_notes.js
@@ -0,0 +1,18 @@
+//var is function scoped
+//let is block scoped
+
+
+//Git- Version control system designed to keep chronological timeline of all your code changes
+/*To traverse file system in terminal:
+cd change directory/folder
+cd .. move up one directory/folder
+ls lists items in folder
+pwd specifies the path you are in (print working directory)
+*/
+
+/* To initialize a git repository
+git init to initialize a git repository*/
+//git status tells you the status of your repository
+/* Staging files for a commit
+git add <file_name> OR git add . to add ALL of the files inside the folder you are in
+*/
